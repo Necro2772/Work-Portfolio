366ce6cda88198fd88eb3cb097f23e7ca3bbe05d
Michael Roberts <50212290+Necro2772@users.noreply.github.com>
2025-12-15 20:25:22 +0200

    Initial commit

---
+++ js/main.ts
@@ -0,0 +1,21 @@
+let themeToggle = document.getElementById("theme-toggle")!;
+
+addEventListener("load", () => {
+    let theme = localStorage.getItem("theme");
+    if (theme != null && theme === "light") {
+        themeToggle.textContent = "light_mode";
+        document.querySelector("html")?.setAttribute("data-theme", "light");
+    }
+})
+
+themeToggle.addEventListener("click", () => {
+    if (localStorage.getItem("theme") === "light") {
+        localStorage.setItem("theme", "dark");
+        themeToggle.textContent = "dark_mode";
+        document.querySelector("html")?.setAttribute("data-theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+        themeToggle.textContent = "light_mode";
+        document.querySelector("html")?.setAttribute("data-theme", "light");
+    }
+})
